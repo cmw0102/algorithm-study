95c4680d3c21b43112c35e95a27db2b0998b4b8b
minwoo Choi <mwplay0102@naver.com>
2024-08-29 16:22:42 +0900

    0829 - 다른 정렬 방법으로 풀고 추후에 커밋 할 예정

---
+++ Python/8월/0829_2750_수-정렬하기.py
@@ -0,0 +1,65 @@
+# 2750_수-정렬하기 - 문제단계: 정렬
+
+# 문제 분석
+# 오름차순 정렬 문제로
+# 첫 번째 입력은 수의 개수
+# 두 번째 입력은 수 입력
+
+# 해결 과정
+# 첫 번째
+# 여러 정렬 방법이 있었지만 다른 정렬 방법보다 간단하게 구현이 가능한
+# 선택 정렬로 풀기로 하였다
+# 선택 정렬은 최솟값을 구하는 방식으로 풀면 된다.
+
+# 선택 정렬 (최종 답안)
+import sys
+input = sys.stdin.readline
+n = int(input())  # 입력할 수의 개수 입력
+n_list = []
+
+for i in range(n):
+    n_list.append(int(input()))  # N번 만큼 수를 입력 받음
+
+for i in range(n-1):
+    min_index = i  # 현재 위치를 최소값의 인덱스로 가정
+    for j in range(i+1, n):
+        if n_list[min_index] > n_list[j]:  # 더 작은 값을 찾으면 min_index를 업데이트
+            min_index = j
+    # 최소값을 현재 위치로 이동
+    n_list[i], n_list[min_index] = n_list[min_index], n_list[i]
+    # 튜플 언패킹
+    # n_list[i] = n_list[min_index]
+    # n_list[min_index] = n_list[i]
+for i in range(n):
+    print(n_list[i])
+
+
+# 선택 정렬 (처음에 본인이 작성한 코드)
+input = sys.stdin.readline
+n = int(input())  # 입력할 수의 개수 입력
+n_list = []
+
+for i in range(n):
+    n_list.append(int(input()))  # N번 만큼 수를 입력 받음
+
+for i in range(n-1):
+    temp = n_list[i]
+    temp2 = i
+    for j in range(i+1, n):
+        if n_list[i] > n_list[j] and temp > n_list[j]:
+            temp = n_list[j]
+            temp2 = j
+    n_list[temp2] = n_list[i]
+    n_list[i] = temp
+
+for i in range(n):
+    print(n_list[i])
+# 결론
+# 선택 정렬을 찾아본 결과 본인이 작성한 코드에서 문제점은 temp와 temp2로 나누어서 값과 인덱스를 따로 설정했다는 것이다.
+# 이로 인해 두 변수 모두 초기화를 한 번씩 해줘야 했다.
+# 이를 해결한 방법은 최종 답안과 같이 인덱스 변수 하나만을 생성하여 값도 인덱스 변수로 구할 수 있게 작성하는 것이다.
+# 결국 현재위치를 최솟값의 인덱스로 가정하는 방향은 같았지만 좀 더 간결하게 작성해야한다.
+# ++ if문 안에 조건문도 복잡함(굳이다 싶은 코드 조합임)
+# [처음 작성한 코드는 런타임 에러가 발생했음]
+# 선택 정렬이라는 개념을 아는 것을 별개로 이를 코드로 구현한다는게 상당히 어렵고 복잡한 것 같다...
+# 열심히 하자.
